605be1cc0a49fc93da0c6e1d2bd342deb1d9e85a
Panya Thitimathanont <panya.thi@gmail.com>
2026-06-29 00:12:00 +0700

    Created Record exercise

--- creating-records-exercise/app.mjs
+++ creating-records-exercise/app.mjs
@@ -8,6 +8,38 @@ app.use(express.json());
 
 // 📍 **** สร้าง API เพื่อใช้ในการเพิ่มข้อมูลหนังเรื่องใหม่ไปที่ Database ตรงนี้ ****
 
+app.post("/movies", async (req, res) =>{
+//1)Access ข้อมูลใน body จาก request ด้วย req.body
+	const newMovie = {
+...req.body,
+
+	}
+// 2) เขียน query เพื่อ  insert ข้อมูลโพสด้วย Pool
+try {
+await pool.query(
+'insert into movies ( title, description, genres, year,poster, rating) values ($1, $2, $3, $4, $5, $6)',
+[
+	
+	newMovie.title,
+	newMovie.description,
+	newMovie.genres,
+	newMovie.year,
+	newMovie.poster,
+	newMovie.rating	
+]
+
+)
+// 30 Return response กลับไปยัง client ว่าได้ทำการเพิ่มข้อมูลเรียบร้อยแล้ว
+
+return res.status(201).json({message:"Movie has been created."})
+
+}catch (error) {
+	return res.status(404).json({message:"error.message"})
+
+}})
+
+
+
 app.listen(port, () => {
 	console.log(`🚀 Server is running at ${port}`);
 });
