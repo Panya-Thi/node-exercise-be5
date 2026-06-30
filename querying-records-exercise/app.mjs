a30594582574ca4aa07bb1d47a176e4737dca755
Panya Thitimathanont <panya.thi@gmail.com>
2026-07-01 00:44:54 +0700

    Querying record exercise

--- querying-records-exercise/app.mjs
+++ querying-records-exercise/app.mjs
@@ -16,6 +16,37 @@ app.get("/movies", async (req, res) => {
 
 // 📍 **** สร้าง API เพื่อใช้ในการดูข้อมูลหนังแต่ละเรื่องด้วย movieId ตรงนี้ ****
 
+app.get("/movies/:movieId", async (req,res) =>{
+
+const movieIdFromClient = req.params.movieId;
+
+const results = await pool.query(
+
+` select * from movies where movie_id =$1`, 
+[movieIdFromClient]
+
+)
+if (!results.rows[0]){
+	return res.status(404).json({
+		message: `"ไม่สามารถเชื่อมต่อ Database ได้" (movie id: ${movieIdFromClient})`
+	})
+
+
+}
+return res.status(200).json({
+message : "Movie has been created.",
+data: results.rows[0]
+
+})
+
+
+
+})
+
+
+
+
+
 app.listen(port, () => {
 	console.log(`🚀 Server is running at ${port}`);
 });
